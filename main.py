04c9d8e4752c1190611096cfc88c794835f581b8
Pratikreddy <83733918+Pratikreddy@users.noreply.github.com>
2024-05-16 12:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import json
 import requests
 from openai import OpenAI
+from groq import Groq
+import base64
+import os
 
 # Set up the page layout
 st.set_page_config(page_title="Model Match", layout="wide")
